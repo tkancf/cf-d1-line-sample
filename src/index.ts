857a700b975851265ce82d73d7a1543119b83d74
tkancf <18424095+tkancf@users.noreply.github.com>
2023-04-21 00:08:34 +0900

    Modified to reply with the same message for LINE messages
    reference: https://zenn.dev/razokulover/articles/4d0ba10083524e#%E3%83%A1%E3%83%83%E3%82%BB%E3%83%BC%E3%82%B8%E3%82%92%E3%81%8A%E3%81%86%E3%82%80%E8%BF%94%E3%81%97%E3%81%99%E3%82%8Bbot%E3%82%92%E4%BD%9C%E3%82%8B

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,61 @@
+import {
+  MessageAPIResponseBase,
+  TextMessage,
+  WebhookEvent,
+} from "@line/bot-sdk";
 import { Hono } from "hono";
- 
-const app = new Hono();
 
+const app = new Hono();
 app.get("*", (c) => c.text("Hello World!"));
 
 app.post("/api/webhook", async (c) => {
-  console.log(JSON.stringify(c));
-  return c.json({ message: "Hello World!" });
+  const data = await c.req.json();
+  const events: WebhookEvent[] = (data as any).events;
+  // @ts-ignore アクセストークンが読めずにエラーになるが、実環境では読めるので無視する
+  const accessToken: string = c.env.CHANNEL_ACCESS_TOKEN;
+
+  await Promise.all(
+    events.map(async (event: WebhookEvent) => {
+      try {
+        await textEventHandler(event, accessToken);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(err);
+        }
+        return c.json({
+          status: "error",
+        });
+      }
+    })
+  );
+  return c.json({ message: "ok" });
 });
+
+const textEventHandler = async (
+  event: WebhookEvent,
+  accessToken: string
+): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return;
+  }
+
+  const { replyToken } = event;
+  const { text } = event.message;
+  const response: TextMessage = {
+    type: "text",
+    text,
+  };
+  await fetch("https://api.line.me/v2/bot/message/reply", {
+    body: JSON.stringify({
+      replyToken: replyToken,
+      messages: [response],
+    }),
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export default app;
