26939c77ff1e199bb952d3f761b6ff4c36b6e55f
tkancf <18424095+tkancf@users.noreply.github.com>
2023-05-05 21:29:59 +0900

    Add LINE reply message to DB

--- src/index.ts
+++ src/index.ts
@@ -45,41 +45,45 @@ app.post("/api/webhook", async (c) => {
   const data = await c.req.json();
   const events: WebhookEvent[] = (data as any).events;
   const accessToken: string = c.env.CHANNEL_ACCESS_TOKEN;
-
-  await Promise.all(
-    events.map(async (event: WebhookEvent) => {
-      try {
-        if (event.type !== "message" || event.message.type !== "text") {
-          return;
-        }
-
-        const { replyToken } = event;
-        const { text } = event.message;
-        const response: TextMessage = {
-          type: "text",
-          text,
-        };
-        await fetch("https://api.line.me/v2/bot/message/reply", {
-          body: JSON.stringify({
-            replyToken: replyToken,
-            messages: [response],
-          }),
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        });
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          console.error(err);
-        }
-        return c.json({
-          status: "error",
-        });
+  const event = events
+    .map((event: WebhookEvent) => {
+      if (event.type != "message" || event.message.type != "text") {
+        return;
       }
+      return event;
     })
-  );
+    .filter((event) => event)[0];
+  // undefinedを除外
+  if (!event) {
+    return c.json({ message: "No event: ${events}" });
+  }
+  const { replyToken } = event;
+  const { text } = event.message as TextMessage;
+
+  // textの内容が「一覧」の場合、DBからデータを取得して、番号を日付順につけて返す
+  if (text === "一覧") {
+    const stmt = await c.env.DB.prepare(`SELECT * FROM shopping_list;`);
+    const allResults: D1Result<ShoppingItem> = await stmt.all();
+    if (!allResults.results) {
+      // undefiled result
+      return c.json({ message: "no results" });
+    }
+
+    const results: { results: ShoppingItem[] } = {
+      results: allResults.results || [],
+    };
+    // convert results to string
+    const message: string = results.results
+      .map((result) => {
+        return `${result.id}: ${result.item}`;
+      })
+      .join("\n");
+    // LINEに返信する
+    const client = new LineClient(accessToken);
+    client.replyMessage(replyToken, message);
+    return c.json({ message: "ok" });
+  }
+
   return c.json({ message: "ok" });
 });
 
